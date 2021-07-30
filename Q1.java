8692a257075e170df9b936b75e585c9cf8a3cfcb
Vathiya Rezky Aliefasyah <79706184+vathxra@users.noreply.github.com>
2021-07-30 07:02:55 +0700

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,26 @@
+public class Q1 {
+    public static void main(String args[]) {
+      int a=6;
+      int b=10;
+      int c=15;
+      System.out.println(a);
+      if(a % 3 == 0)
+      {
+        System.out.println("Hello");
+        System.out.println("\n");
+      }
+        
+        System.out.println(b);
+        if(b % 5 == 0)
+        {
+            System.out.println("World");
+            System.out.println("\n");
+        }
+        
+        System.out.println(c);
+        if(b % 5 == 0 && b % 5 == 0)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
